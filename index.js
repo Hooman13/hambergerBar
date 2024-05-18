aa22e4edcfb2bb7c0c3996b6e9bc67c807896fb2
Hooman <hooman.torabi18@gmail.com>
2024-05-19 01:04:09 +0330

    correct hamburgerBar

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const hamMenu = document.querySelector("#hamburger");
-const fullMenu = document.querySelector(".fullMenu");
+const fullMenu = document.querySelector("#mobileSize");
 function replaceFunction() {
   hamMenu.classList.replace("sm:hidden", "hidden");
-  fullMenu.classList.replace("hidden", "flex");
+  fullMenu.classList.replace("hidden", "sm:hidden");
 }
 hamMenu.addEventListener("click", replaceFunction);
