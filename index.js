0447a40a67812b6ce676507dfdc572aa61ef551c
Hooman <hooman.torabi18@gmail.com>
2024-05-19 00:26:03 +0330

    newedit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const hamMenu = document.querySelector(".hamburger");
-const fullMenu = document.querySelector("fullMenu");
+const hamMenu = document.querySelector("#hamburger");
+const fullMenu = document.querySelector(".fullMenu");
 function replaceFunction() {
   hamMenu.classList.replace("sm:hidden", "hidden");
   fullMenu.classList.replace("hidden", "flex");
